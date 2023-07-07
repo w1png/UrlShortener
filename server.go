c0f988f81ebfdf3bb5674719d57f3872154df18c
Denis Smirnov <w1png@yandex.ru>
2023-07-07 18:40:30 +0300

    Add CORS

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -19,7 +20,11 @@ func NewApiServer(port string) *ApiServer {
 }
 
 func (s *ApiServer) Run() error {
-  return http.ListenAndServe(s.listenAddr, s.router)
+  return http.ListenAndServe(s.listenAddr, handlers.CORS(
+    handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+    handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+    handlers.AllowedOrigins([]string{"*"}),
+  )(s.router))
 }
 
 func (s *ApiServer) RegisterHandlerFunc(path string, f func(w http.ResponseWriter, r *http.Request), method string) {
